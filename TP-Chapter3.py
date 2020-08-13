fd9db3aea73ce7fc647ff4674139b46f21b5f703
JHJee <chlwlgp132@gmail.com>
2020-08-13 21:54:46 +0900

    second commit

---
+++ TP-Chapter3.py
@@ -0,0 +1,15 @@
+# Chapter 3.6
+# Example code - lyrics function calls
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+def print_lyrics():
+    print("yes")
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+
+repeat_lyrics()
+
