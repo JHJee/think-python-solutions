ee62e72cb885215f9f6da1663df739133f20a319
JHJee <chlwlgp132@gmail.com>
2020-08-15 10:39:44 +0900

    add chapter5

--- TP-Chapter3.py
+++ TP-Chapter3.py
@@ -1,15 +1,67 @@
-# Chapter 3.6
-# Example code - lyrics function calls
-def repeat_lyrics():
-    print_lyrics()
-    print_lyrics()
+def exercise_1():
+    print("# exercise_1()")
+    # Traceback (most recent call last):
+    #  File "c:\Users\Jihye\Desktop\TP-Chapter3.py", line 3, in <module>
+    #    repeat_lyrics()
+    # NameError: name 'repeat_lyrics' is not defined
+    pass
 
 
-def print_lyrics():
-    print("yes")
-    print("I'm a lumberjack, and I'm okay.")
-    print("I sleep all night and I work all day.")
+def exercise_2():
+    print("\n# exercise_2()")
+
+    def repeat_lyrics():
+        print_lyrics()
+        print_lyrics()
+
+    def print_lyrics():
+        print("yes")
+        print("I'm a lumberjack, and I'm okay.")
+        print("I sleep all night and I work all day.")
+
+    repeat_lyrics()
 
 
-repeat_lyrics()
+def exercise_3(str):
+    print("\n# exercise_3()")
+    print(" " * 65, str)
+
+
+def exercise_4():
+    print("\n# exercise_4()")
+
+    def print_spam(str):
+        print(str)
+
+    def do_twice(f, str):
+        f(str)
+        f(str)
+
+    def do_four(f, str):
+        do_twice(f, str)
+        do_twice(f, str)
+
+    do_four(print_spam, "spam")
+
+
+def exercise_5():
+    print("\n# exercise_5()")
+    print("+", "----", "+", "----", "+")
+    print("|      |      |")
+    print("|      |      |")
+    print("|      |      |")
+    print("|      |      |")
+    print("+", "----", "+", "----", "+")
+    print("|      |      |")
+    print("|      |      |")
+    print("|      |      |")
+    print("|      |      |")
+    print("+", "----", "+", "----", "+")
+
+
+exercise_1()
+exercise_2()
+exercise_3("allen")
+exercise_4()
+exercise_5()
 
---
+++ TP-Chapter5.py
@@ -0,0 +1,21 @@
+def exercise_3():
+    pass
+
+
+def exercise_4():
+    pass
+
+
+def exercise_5():
+    pass
+
+
+def exercise_6():
+    pass
+
+
+exercise_3()
+exercise_4()
+exercise_5()
+exercise_6()
+
